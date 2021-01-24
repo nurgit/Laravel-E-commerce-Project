fc39ecfa60ff12e8c959718cb6fb5d2b8971675b
nurgit <nurmohammad.aiub@gmail.com>
2021-01-25 00:44:45 +0600

    add AdminTableSeeder data

--- database/seeds/AdminTableSeeder.php
+++ database/seeds/AdminTableSeeder.php
@@ -11,6 +11,15 @@ class AdminTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB:table('admins')->delete();
+        $adminRecords=[
+            [
+                'id'=>1,'name'=>'admin','type'=>'admin','mobil'=>'01303156230','email'=>'nur80659@gmail.com'
+                ,'password'=>'','image'=>'','status'=>1,
+            ],
+        ];
+        foreach ($adminRecords as $key => $record){
+            \App\Admin::create($record);
+        }
     }
 }
